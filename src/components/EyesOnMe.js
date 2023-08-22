609699ca828e4d7efe2c3925b54fc41061699294
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-08-21 21:24:27 -0600

    Finished Lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function CodeEyesOnMe() {
+
+    function buttonOnFocusHandler() {
+        console.log("Good!");
+    }
+
+    function buttonOnBlurHandler() {
+        console.log("Hey! Eyes on me!");
+    }
+
+    
+    return (
+        <button onFocus={buttonOnFocusHandler} onBlur={buttonOnBlurHandler}>Eyes on me</button>
+    );
+}
+
+export default CodeEyesOnMe;
